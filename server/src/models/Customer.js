6e259fd06f80b9711ef32411ab98773b4de53f05
ALHAJI ALI MOHAMMED <libaski2013@yahoo.com>
2026-09-10 12:25:52 +0000

    Add customer profile address field

--- server/src/models/Customer.js
+++ server/src/models/Customer.js
@@ -11,6 +11,7 @@ const customerSchema = new mongoose.Schema(
     lastVisit: { type: Date },
     email: String,
     location: { type: String, trim: true },
+    address: { type: String, trim: true },
     branch: { type: mongoose.Schema.Types.ObjectId, ref: 'Branch', index: true },
     outlet: { type: mongoose.Schema.Types.ObjectId, ref: 'Outlet', index: true },
     active: { type: Boolean, default: true },
